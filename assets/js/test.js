454a2cb69359f478eef7455b26ef230793d9defa
Nathan Lima <nathanls.dev@gmail.com>
2024-01-25 21:27:51 -0300

    style: rewriting css - user-presentation done

---
+++ assets/js/test.js
@@ -0,0 +1,9 @@
+// const batata = document.querySelector("main");
+// const aqui = document.querySelector("#aqui");
+
+// aqui.addEventListener("click", () => {
+//   batata.classList.remove("batata");     
+
+//   // Adiciona uma borda vermelha
+//   batata.style.border = "1px solid red";
+// });
