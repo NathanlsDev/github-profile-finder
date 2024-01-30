5f298b5f160b508034c588e6bbe5f982c7644dca
Nathan Lima <nathanls.dev@gmail.com>
2024-01-29 21:45:07 -0300

    core: removed unnecessary script archive

--- assets/js/test.js
+++
@@ -1,9 +0,0 @@
-// const batata = document.querySelector("main");
-// const aqui = document.querySelector("#aqui");
-
-// aqui.addEventListener("click", () => {
-//   batata.classList.remove("batata");     
-
-//   // Adiciona uma borda vermelha
-//   batata.style.border = "1px solid red";
-// });
