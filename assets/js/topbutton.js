6727992090c6752b3af15746a739d426c44d2938
Nathan Lima <nathanls.dev@gmail.com>
2024-02-02 19:56:27 -0300

    refactor: button return to page top

--- assets/js/topbutton.js
+++ assets/js/topbutton.js
@@ -1,12 +1,11 @@
-const returnBtn = document.querySelector('.btnTop')
+const returnBtn = document.querySelector(".btnTop");
 
-returnBtn.addEventListener('click', () => {
-  window.scrollTo(0,0)
-})
+returnBtn.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
 
-document.addEventListener('scroll', () => {
-  if(window.scrollY > 200){
-    return returnBtn.style.display = 'flex'
-  }  
-  return returnBtn.style.display = 'none'
-})
+document.addEventListener("scroll", () => {
+  return window.scrollY > 200
+    ? (Btn.style.display = "flex")
+    : (Btn.style.display = "none");
+});
